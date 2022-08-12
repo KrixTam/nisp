d80bf63ef3d92e836720d47a643c4783ebf86612
Krix Tam <krix.tam@qq.com>
2022-08-12 10:27:38 +0800

    cli: NISPClient重构end和reconnect，新增connect和close方法

--- nisp/cli/core.js
+++ nisp/cli/core.js
@@ -53,73 +53,29 @@ HEARTBEAT[constants.HEARTBEAT_ID] = function (data) {};
 
 init_commands(HEARTBEAT);
 
-var NISPClient = function (fd_path) {
-	var that = this;
+var NISPClient = function (fd_path, heartbeat_interval=5000) {
 	this.connected = false;
 	this.name = null;
+	this.heartbeat_interval = heartbeat_interval;
 	// this.heartBeatList = new Array();
 	this._processings = {};
 	this._emitter = new events.EventEmitter();
 	this._fd = { path: fd_path };
-	this._client = net.createConnection(this._fd);
-	this._client.on('data', function (data) {
-		console.log('======= 2222 ========');
-		console.log(data.toString());
-		let response_content = JSON.parse(data.toString());
-		if (constants.KEY_NAME in response_content) {
-			that.name = response_content.name;
-		} else {
-			if ((constants.KEY_EVENT_ID in response_content) && (constants.KEY_ERROR_CODE in response_content) && (constants.KEY_DATA in response_content)) {
-				let [cid, state, timestamp] = datamodel.unpack_event_id(response_content.eid);
-				logger.info([999, data.toString()]);
-				let state_value = parseInt(state, 2);
-				let event_index = generate_event_index(cid, timestamp);
-				if (cid == constants.HEARTBEAT_ID) {
-					if (state_value == constants.STATE_PROCESS_END) {
-						that._emitter.emit(event_index);	
-					}
-				} else {
-					if (state_value == constants.STATE_INIT_END) {
-						that._emitter.emit(event_index, response_content.data).then()
-					} else {
-						if (state_value == constants.STATE_PROCESS_END) {
-
-						}
-					}
-				}
-				
-				if (event_index in that._processings) {
-					that._processings[event_index] = content;
-					that._emitter.emit(event_index);
-				}
-			} else {
-				logger.warn([1200, data.toString()]);
-			}
-		}
-		
-	});
-	this._client.on('connect', function () {
-		that.connected = true;
-		// that.heartbeat();
-		logger.info([1000]);
-	});
-	this._client.on('end', function () {
-		that.connected = false;
-		// that.clearHeartBeat();
-		logger.warn([1203]);
-	});
-	/* istanbul ignore next */
-	this._client.on('error', function (err) {
-		logger.error([1201, err]);
-	});
+	this._client = null;
+	this.connect();
 };
 
 NISPClient.prototype.heartbeat = function () {
-	var that = this;
-	let evt = new datamodel.Event(constants.HEARTBEAT_ID);
-	this.send(evt, this.name, () => {
-		setTimeout(that.heartbeat(), 10000);
-	});
+	if (this.connected) {
+		console.log('j88888888888');
+		var that = this;
+		let evt = new datamodel.Event(constants.HEARTBEAT_ID);
+		this.send(evt, that.name, () => {
+			setTimeout(that.heartbeat, that.heartbeat_interval);
+		});
+	} else {
+		logger.warn([1206]);
+	}
 };
 
 NISPClient.prototype.send = function (event, data, callback) {
@@ -130,19 +86,76 @@ NISPClient.prototype.send = function (event, data, callback) {
 	// 	let [cid, state_bin, ts] = datamodel.unpack_event_id(event_id);
 	// 	evt = new datamodel.Event(cid, state_bin, ts);
 	// }
+	console.log(data);
 	let content = event.process(data);
 	if (null != content) {
 		let event_index = generate_event_index(event);
 		this._processings[event_index] = null;
+		console.log('999999999999999');
+		logger.info([999, content]);
 		this._client.write(content);
 		this._emitter.once(event_index, callback);
 	}
 };
 
-NISPClient.prototype.reconnect = function () {
+NISPClient.prototype.connect = function () {
 	if (this.connected == false) {
-		this._client.connect(this._fd);	
+		var that = this;
+		this._client = net.createConnection(this._fd)
+		.on('data', function (data) {
+			console.log('======= 2222 ========');
+			// console.log(data.toString());
+			let response_content = JSON.parse(data.toString());
+			if (constants.KEY_NAME in response_content) {
+				that.name = response_content.name;
+				console.log(that.heartbeat_interval);
+				that.heartbeat();
+			} else {
+				if ((constants.KEY_EVENT_ID in response_content) && (constants.KEY_ERROR_CODE in response_content) && (constants.KEY_DATA in response_content)) {
+					let [cid, state, timestamp] = datamodel.unpack_event_id(response_content.eid);
+					logger.info([999, data.toString()]);
+					let state_value = parseInt(state, 2);
+					let event_index = generate_event_index(cid, timestamp);
+					if (cid == constants.HEARTBEAT_ID) {
+						console.log('state.......');
+						console.log(state_value);
+						if (state_value == constants.STATE_PROCESS_END) {
+							that._emitter.emit(event_index);	
+						} else {
+							logger.warn([1205]);
+							that.end();
+						}
+					} else {
+						if (state_value == constants.STATE_INIT_END) {
+							that._emitter.emit(event_index, response_content.data).then()
+						} else {
+							if (state_value == constants.STATE_PROCESS_END) {
+
+							}
+						}
+					}
+				} else {
+					logger.warn([1200, data.toString()]);
+				}
+			}
+		}).on('connect', function () {
+			that.connected = true;
+			logger.info([1000]);
+		}).on('end', function () {
+			that.connected = false;
+			// that.clearHeartBeat();
+			logger.warn([1203]);
+		}).on('error', function (err) {
+			/* istanbul ignore next */
+			that.connected = false;
+			logger.error([1201, err]);
+		});
 	}
+};
+
+NISPClient.prototype.reconnect = function () {
+	this._client.destroy();
+	this.connect();
 };
 
 // NISPClient.prototype.clearHeartBeat = function () {
@@ -156,11 +169,11 @@ NISPClient.prototype.end = function() {
 	this._client.end();
 };
 
-// NISPClient.prototype.close = function() {
-// 	this.connected = false;
-// 	this._client.close();
-// 	this._client.destroy();
-// };
+NISPClient.prototype.close = function() {
+	this.end();
+	this._client.destroy();
+	this._client = null;
+};
 
 // var config = require('./config').config;
 // var nispClient = new NISPClient(config.socket);
--- nisp/cli/test/core.test.js
+++ nisp/cli/test/core.test.js
@@ -7,6 +7,9 @@ const core = require('../core');
 const constants = require('../constants');
 
 
+// jest.setTimeout(10000);
+
+
 function delay(time) {
 	return new Promise(resolve => setTimeout(resolve, time));
 };
@@ -92,15 +95,14 @@ describe('测试once', () => {
 
 describe('测试NISPClient', () => {
 
-	test('connect', done => {
-		let server = net.createServer();
+	test('connect测试', done => {
 		let fd_path = './test.client.01.sock';
-		server.listen(fd_path);
-		server.on('connection', function (socket) {
+		let server = net.createServer().listen(fd_path);
+		server.on('connection', (socket) => {
 			socket.write('{"name": "test_abc"}');
 			setTimeout(() => {
 				expect(client.name).toBe('test_abc');
-				setTimeout(async () => {
+				setTimeout(() => {
 					socket.end();
 					server.close();
 					done();
@@ -110,53 +112,51 @@ describe('测试NISPClient', () => {
 		let client = new core.NISPClient(fd_path);
 	});
 
-	test('end', done => {
+	test('end测试', done => {
 		let count = 0;
-		let server = net.createServer((stream) => {
-			stream.on('end', () => {
+		let fd_path = './test.client.02.sock';
+		let server = net.createServer((socket) => {
+			socket.on('end', () => {
+				console.log('8778787878')
 				count = count - 1;
 			});
-		});
-		let fd_path = './test.client.02.sock';
-		server.listen(fd_path);
-		server.on('connection', function (socket) {
-			socket.write('{"name": "test_abc"}');
-			count = count + 2;
-			setTimeout(() => {
+		}).listen(fd_path);
+		server.on('connection', (socket) => {
+				count = count + 2;
 				client.end();
-				setTimeout(async () => {
+				setTimeout(() => {
 					server.close();
 					expect(count).toBe(1);
 					done();
 				}, 200);
-			}, 200);
-		});
+			});
 		let client = new core.NISPClient(fd_path);
 	});
 
-	test('reconnect', done => {
+	test('reconnect测试', done => {
 		let count = 0;
-		let server = net.createServer((stream) => {
-			stream.on('end', () => {
+		let fd_path = './test.client.03.sock';
+		let flag = true;
+		let server = net.createServer((socket) => {
+			socket.on('end', () => {
+				console.log('4455666644444');
 				count = count - 3;
 			});
-		});
-		let fd_path = './test.client.03.sock';
-		server.listen(fd_path);
-		let flag = true;
-		server.on('connection', function (socket) {
-			socket.write('{"name": "test_abc"}');
+		}).listen(fd_path);
+		server.on('connection', (socket) => {
 			count = count + 4;
 			if (flag) {
 				flag = false;
-				socket.end();
+				console.log('444444444');
+				client.end();
 				setTimeout(() => {
 					client.reconnect();
 				}, 200);
 			} else {
-				setTimeout(async () => {
+				socket.write('{"name": "test_abc"}');
+				setTimeout(() => {
 					server.close();
-					client.end();
+					client.close();
 					expect(count).toBe(5);
 					done();
 				}, 200);
@@ -164,6 +164,54 @@ describe('测试NISPClient', () => {
 		});
 		let client = new core.NISPClient(fd_path);
 	});
+
+	// test('heartbeat测试', done => {
+	// 	let count = 0;
+	// 	let fd_path = './test.client.03.sock';
+	// 	let server = net.createServer((stream) => {
+	// 		stream.on('data', (msg) => {
+	// 			if (count < 4) {
+	// 				console.log('333333333');
+	// 				console.log(msg.toString());
+	// 				let req = JSON.parse(msg.toString());
+	// 				let [cid, state_bin, ts] = datamodel.unpack_event_id(req.eid);
+	// 				expect(cid).toBe(constants.HEARTBEAT_ID);
+	// 				let evt = new datamodel.Event(cid, constants.STATE_PROCESS_END, ts);
+	// 				let res = {};
+	// 				res[constants.KEY_EVENT_ID] = evt.eid();
+	// 				res[constants.KEY_DATA] = {};
+	// 				res[constants.KEY_ERROR_CODE] = 0;
+	// 				stream.write(JSON.stringify(res));
+	// 				count = count + 1;		
+	// 			}
+	// 		});
+	// 	}).listen(fd_path);
+	// 	// let fn = function () {
+	// 	// 	setTimeout(() => {
+	// 	// 		console.log('5555555555');
+	// 	// 		if (count < 4) {
+	// 	// 			fn();
+	// 	// 		} else {
+	// 	// 			setTimeout(async () => {
+	// 	// 				client.end();
+	// 	// 				server.close();
+	// 	// 				expect(count).toBe(4);
+	// 	// 				done();
+	// 	// 			}, 200);
+	// 	// 		}
+	// 	// 	}, 200);
+	// 	// };
+	// 	server.on('connection', function (socket) {
+	// 		socket.write('{"name": "test_abc"}');
+	// 		setTimeout(async () => {
+	// 			client.end();
+	// 			server.close();
+	// 			expect(count).toBe(4);
+	// 			done();
+	// 		}, 2400);
+	// 	});
+	// 	let client = new core.NISPClient(fd_path, 500);
+	// });
 
 });
 
